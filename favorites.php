4b16db10dd0a70bbef18c0ba581f97e9eaf2e506
Arjun Mitra <mitraarj@usc.edu>
2021-12-08 16:17:09 -0800

    updated jquery link

--- favorites.php
+++ favorites.php
@@ -154,14 +154,13 @@
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         
        // do jquery ajax call here 
 
         $( ".remove-btn" ).on( "click", function() {
-            console.log("in remove");
-            console.log($(this).parent().parent());
-            console.log($(this).data("track"));
+            
             let $parent = $(this).parent().parent();
             $.get( "remove.php", { track_id: $(this).data("track") } )
             .done(function( data ) {
